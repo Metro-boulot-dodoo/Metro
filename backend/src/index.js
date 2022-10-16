bf75b5d4f270ea03bd2635550d296004f229fcb6
flolgl <florianlegal@outlook.fr>
2022-10-16 19:42:08 +0200

    Remove ignored files

--- backend/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// source code for the backend
-exports.__esModule = true;
-var File_1 = require("./File/File");
-var getFile = function (fileName) {
-    var file = File_1.File.read(fileName);
-    console.log(file);
-};
-getFile('public/data/metro.txt');
